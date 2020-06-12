9cfeeb6e8aa9d25752bd8ace6357fe206d99b05c
Bryan Cross <bryan@thecrossfamily.org>
2020-06-12 10:20:29 -0500

    Added logging Fixed cert-manager static version

--- Deploy.ps1
+++ Deploy.ps1
@@ -3,6 +3,7 @@ param (
     [Parameter(Mandatory = $true)]
     [string] $Hostname
 )
+Write-Host "Waiting to connect to the cluster..."
 .\kubectl version *>$null
 while (!$?){
     Start-Sleep -seconds 5
@@ -12,9 +13,9 @@ while (!$?){
 .\helm repo add jetstack https://charts.jetstack.io
 .\helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
 .\helm repo update
-.\kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.15.1/cert-manager.crds.yaml
+.\kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/latest/download/cert-manager.crds.yaml
 .\kubectl create namespace cert-manager
-.\helm install cert-manager jetstack/cert-manager --namespace cert-manager --version v0.15.1 --wait
+.\helm install cert-manager jetstack/cert-manager --namespace cert-manager --wait
 .\kubectl create namespace cattle-system
 .\helm install rancher rancher-stable/rancher --namespace cattle-system --set hostname=$hostname
 .\kubectl -n cattle-system rollout status deploy/rancher
